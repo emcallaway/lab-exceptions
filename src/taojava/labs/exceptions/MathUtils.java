fa26501f681d31af98079ce628666ca23477ff60
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2013-09-15 20:50:58 -0500

    First real version of this code.

---
+++ src/taojava/labs/exceptions/MathUtils.java
@@ -0,0 +1,11 @@
+package taojava.labs.exceptions;
+
+/**
+ * A variety of utilities for doing Math.
+ * 
+ * @author Samuel A. Rebelsky
+ * @author Your Name Here
+ */
+public class MathUtils {
+
+} // MathUtils
